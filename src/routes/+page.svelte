e9a99a5e642cec02dd6008471370e5ab5655ba31
KibiLab <126893951+KibiLab@users.noreply.github.com>
2023-03-31 22:38:22 +0700

    1 file modified, 2 deleted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,4 @@
-<script>
-	
-</script>
 <h1 class="text-3xl font-bold">
   Hello world!	
 </h1>
-<button class="btn btn-secondary">Click Button</button>
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.blue.100);
-  }
-</style>
+<button class="btn btn-secondary">Click Me!</button>
