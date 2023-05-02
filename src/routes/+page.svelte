b2e8968ef7ad0d446312e020a01bb42fe592149b
KibiLab <126893951+KibiLab@users.noreply.github.com>
2023-05-02 23:32:34 +0700

    1 file modified

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,55 @@
 <script>
+// Function to move element in array
+function arrayMove(arr, fromIndex, toIndex) {
+  var element = arr[fromIndex];
+  arr.splice(fromIndex, 1);
+  arr.splice(toIndex, 0, element);
+}
+
+// Convert string to array
+let str = '["a", "b", "c"]';
+let newArr = JSON.parse(str);
+console.log("New Array is: ", newArr);
+console.log(newArr[0]);
+
 let page = [
-  {"s1":{
+  {
     "type":"div",
     "include":"abc ksjd fkj kfj kdf",
     "contain":"s1_1 s1_2 s1_3",
     "px":"px-10",
     "py":"py-10",
-  }},
-  {"s2":{
+  },
+  {
     "type":"img",
     "include":"abc ksjd fkj kfj kdf",
     "contain":"s1_1 s1_2 s1_3",
     "px":"px-10",
     "py":"py-10",
-  }}
+  }
 ]
 
-console.log(page[0]);
-console.log(page[0].s1);
-if(page[1].s2){
-  console.log(page[1].s2.type);
-}
+var strobj = JSON.stringify(page);
+var unstrobj = JSON.parse(strobj);
+
 </script>
 <div class="px-10 py-10">  
   <h1 class="text-3xl py-8 px-8 font-bold">Hello World!</h1>
-  <p>{page[0].s1.type}</p>
-  <p>{page[0].s1.include}</p>
-  <p>{page[0].s1.contain}</p>
-  <p>{page[0].s1.px}</p>
-  <p>{page[0].s1.py}</p>
+  <p>{page[0].type}</p>
+  <p>{page[0].include}</p>
+  <p>{page[0].contain}</p>
+  <p>{page[0].px}</p>
+  <p>{page[0].py}</p>
+</div>
+<div class="px-10 py-10">  
+  <h1 class="text-3xl py-8 px-8 font-bold">JSON.stringify</h1>
+  <p>{strobj}</p>  
+</div>
+<div class="px-10 py-10">  
+  <h1 class="text-3xl py-8 px-8 font-bold">JSON.parse</h1>
+  <p>{unstrobj[1].type}</p>
+  <p>{unstrobj[1].include}</p>
+  <p>{unstrobj[1].contain}</p>
+  <p>{unstrobj[1].px}</p>
+  <p>{unstrobj[1].py}</p> 
 </div>
